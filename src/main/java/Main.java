51409ed42cc8c49d8544d2cf12a8468df22def5d
François <francois.duguay-giguere.1@ulaval.ca>
2019-04-05 17:32:17 -0400

    cors et port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,47 @@
 import static spark.Spark.*;
 
+import java.io.
+
+import javaslang.control.Try;
+
 public class Main {
     public static void main(String[] args) {
+
+        Integer portNumber = Try.of(() -> Integer.valueOf(System.getenv("PORT"))).orElseGet((t) -> {
+            System.out.println("WARNING: The server port could not be found with 'PORT' env var. Using the " +
+                    "default one (9090)");
+            return 9090;
+        });
+
+        port(Integer.parseInt(System.getenv("PORT")));
+
         get("/", (req,res) -> "Hello World!");
+
+        enableCORS("*", "*", "*");
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            response.type("application/json");
+        });
     }
 }
